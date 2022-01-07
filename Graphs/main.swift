f36a1d33649caaf9ba41519ea4606f49cd600f78
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-01-07 15:53:26 -0800

    breadth first search code example added

--- Graphs/main.swift
+++ Graphs/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /*
  MARK: Graphs
- Used for mapping: social networks, connecting gamers, actual maps, etc.
+ Used for mapping: social networks, connecting gamers to nearby servers, music recommendations, actual maps, etc.
  A binary tree is a very simple graph structure. More complex graphs have the following structure:
  
  A graph (G) is an ordered pair of a set of vertices (V) and a set of edges (E). G = (V,E)
@@ -78,3 +78,147 @@ import Foundation
  Advantage: Fast lookup, easy to find neighbors
  
  */
+
+//MARK: Queue for BFS
+struct Queue<T> {
+    private var array: [T]
+    
+    init() {
+        array = []
+    }
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var count: Int {
+        return array.count
+    }
+    
+    mutating func add(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func remove() -> T? {
+        if isEmpty {
+            return nil
+        } else {
+            return array.removeFirst()
+        }
+    }
+    
+    func peek() -> T? {
+        return array.first
+    }
+}
+
+//MARK: Stack for DFS
+struct Stack<T> {
+    fileprivate var array = [T]()
+    
+    var isEmpty: Bool {
+        return array.isEmpty
+    }
+    
+    var count: Int {
+        return array.count
+    }
+    
+    mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        return array.popLast()
+    }
+    
+    var top: T? {
+        return array.last
+    }
+}
+
+//MARK: Build Graph class
+
+class Graph {
+    var V = 0                       // number of vertices
+    var adj = [[Int]]()             // adjacency list
+    
+    init(_ V: Int) {
+        self.V = V
+        for _ in 0..<V {
+            adj.append([Int]())     // create empty array of adjacency lists
+        }
+    }
+    
+    func addEdge(v: Int, w: Int) {
+        adj[v].append(w)            // add edges
+    }
+
+
+/*
+ MARK: Breadth First Search
+ Start from middle and expand outward to nearest neighbors. Once all same-level nodes have been traverse, next level is searched. Ex: how social media connects to first level contacs, second level, etc.
+ 
+ Uses a queue for search (FIFO). Origin node is popped into and out of queue to establish starting point (special case). All immediately connected nodes are then added to queue and marked as visited. Pop out first value currently in queue and find its neighbors. Go down queue and pop out any nodes that have no unsearched connections. Repeat.
+ */
+
+    func BFS(s: Int) -> [Int] {
+        var result = [Int]()
+    
+        // mark all vertices as not visited
+        var visited = adj.map {_ in false}
+        
+        // create BFS queue
+        var queue = Queue<Int>()
+        
+        // mark first vertex as visit and enqueue
+        visited[s] = true
+        print("Starting at \(s)")
+        queue.add(s)
+        result.append(s)
+        
+        while queue.count > 0 {
+            let current = queue.remove()!
+            print("De-Queueing \(current)")
+            
+            // get all adjacent vertices of the current vertex
+            //if adjacent has not been visited, mark visited and enqueue
+            
+            for n in adj[current] {
+                if visited[n] == false {
+                    visited[n] = true
+                    print("Queuing at \(n)")
+                    queue.add(n)
+                    result.append(n)
+                }
+            }
+        }
+        return result
+    }
+}
+
+
+
+/*
+ MARK: Depth First Search
+ Ignore current depth, and just tunnel downward along single path until end is reached. Ex: Path finding, cycle detection
+ 
+ */
+
+
+
+
+//MARK: Create graph from lesson (non-directional)
+
+let g = Graph(8)
+g.addEdge(v: 0, w: 1)
+g.addEdge(v: 1, w: 4)
+g.addEdge(v: 4, w: 6)
+g.addEdge(v: 6, w: 0)
+g.addEdge(v: 1, w: 5)
+g.addEdge(v: 5, w: 3)
+g.addEdge(v: 3, w: 0)
+g.addEdge(v: 5, w: 2)
+g.addEdge(v: 2, w: 7)
+
+print("Breadth First Search: \(g.BFS(s: 6))")
